3e30d868a9c49197f22aa4f2ddc74c66094daafb
PavloLevchenko <teasetrnet@gmail.com>
2022-01-13 01:19:50 +0200

    Добавляет начальные файлы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 import './sass/main.scss';
+import './js/modal-film';
+// Импорт HTTP клиента
+import axios from 'axios';
+// Импорт библиотеки пагинации
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+// Импорт библиотеки спиннера
+import spin from 'spin';
+// Импорт библиотеки уведомлений
+import Notiflix from 'notiflix';
+
+//состояние страницы
+let state = 'idle'; // "idle", "loading", "find", "error"
+//текущий вид страницы
+let view = 'main'; //"main", "libary"
+
+let page = 1;
+let maxPages = 1;
+let error = '';
+let query = '';
+const LIMIT = 20;
+
+if (state == 'idle') {
+  //начальное состояние
+}
+
+if (state == 'loading') {
+  //показываем загрузчик
+}
+
+if (state == 'find') {
+  //грузим галерею
+}
+
+if (state == 'error') {
+  //показываеем ошибки
+}
