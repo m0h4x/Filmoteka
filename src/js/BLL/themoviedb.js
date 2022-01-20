af26cd7e48bd9f83412d68aca628ef7712ebb183
sergiosergio2 <vzdor5555@gmail.com>
2022-01-20 14:03:57 +0200

    add genres helper for static functions

--- src/js/BLL/themoviedb.js
+++ src/js/BLL/themoviedb.js
@@ -2,19 +2,19 @@ import { API } from './const';
 import axios from 'axios';
 
 export const ApiService = class {
-  constructor() {}
+  
 
-  async fetchTrendingFilms(page = 1) {
+ static async fetchTrendingFilms(page = 1) {
     const response = await axios.get(
       `${API.BASIC_URL}/3/trending/movie/day?api_key=${API.KEY}&page=${page}`,
     );
     return response.data;
   }
-  async getGenres() {
+ static async getGenres() {
     const response = await axios.get(`${API.BASIC_URL}/3/genre/movie/list?api_key=${API.KEY}`);
     return response.data.genres;
   }
-  async fetchMoviesResultsByName(page = 1, searchQuery = '') {
+ static async fetchMoviesResultsByName(page = 1, searchQuery = '') {
     const response = await axios.get(
       `${API.BASIC_URL}/3/search/movie?api_key=${API.KEY}&language=en-US&query=${searchQuery}&page=${page}`,
     );
--- src/js/topFilmsComponent.js
+++ src/js/topFilmsComponent.js
@@ -1,17 +1,17 @@
 import { ApiService } from './BLL/themoviedb';
-import { genresHelper } from './BLL/genresHelper';
-const api = new ApiService();
+import { GenresHelper } from './BLL/genresHelper';
+// const api = new ApiService();
 
 export default function renderTopFilms(page = 1, apiReady, apiError) {
   
-  api
+  ApiService
     .fetchTrendingFilms(page)
     .then(data => {
       const { total_pages, total_results, results } = data;
-      api
+      ApiService
         .getGenres()
         .then(genres => {
-          apiReady(genresHelper.mixGenres(genres, results), total_results);
+          apiReady(GenresHelper.mixGenres(genres, results), total_results);
         })
         .catch(error => {
           apiError(error);
