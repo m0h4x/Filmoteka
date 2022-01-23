e1b95aaf01f2984e23618e583b2b804e909dd25f
lanitochka <70783335+lanitochka@users.noreply.github.com>
2022-01-23 03:01:31 +0100

    improved LocalStorage

--- src/js/filmCard.js
+++ src/js/filmCard.js
@@ -15,27 +15,15 @@ const decorGenres = genres => {
 
 const filmCard = film => {
   const baseImgUrl = 'https://image.tmdb.org/t/p/w342';
-  const {
-    id,
-    poster_path,
-    title,
-    original_title,
-    vote_average,
-    vote_count,
-    overview,
-    popularity,
-    genres,
-    release_date,
-  } = film;
+  const { id, poster_path, title, genres, release_date } = film;
   const image = poster_path ? baseImgUrl + poster_path : defaultImage;
   const filmGenres = decorGenres(genres);
   return `<li class="film__card" data-modal-id=${id}>
   <a class="film__link" href="#">
     <img class="film__cover" src="${image}" alt="${title}" />
-    <h3 class="film__title" data-orig-title="${original_title}">${title}</h3>
-    <div class="film__info" data-vote-average="${vote_average}" data-vote-count="${vote_count}"
-      data-popularity="${popularity}" data-overview="${overview}">
-      <span class="film__genre" data-genres="${genres.join(', ')}">${filmGenres}</span>
+    <h3 class="film__title">${title}</h3>
+    <div class="film__info">
+      <span class="film__genre">${filmGenres}</span>
       <span class="film__year">&nbsp${release_date}</span>
     </div>
   </a>
